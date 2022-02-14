e444afc5cce0ac8cb7ee2265429a8684d5a5a8e8
Kwizera Emmanuel <kwizemmy2004@gmail.com>
2022-02-14 12:27:17 +0000

    1st phase done

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -20,7 +20,7 @@ export let id=''
 
 function keyPress(ev){
 	//only respond if the current modal is the top one
-	if(ev.key=="Escape" && onTop==topDiv) close() //ESC
+	if(ev.key=="Escape" && onTop==topDiv) close(topDiv) //ESC
 }
 
 /**  API **/
@@ -58,9 +58,9 @@ onDestroy(()=>{
 	
 </script>
 
-<div id="topModal" class:visible bind:this={topDiv} on:click={()=>close()}>
+<div id="topModal" class:visible bind:this={topDiv} on:click={()=>close(topDiv)}>
 	<div id='modal' on:click|stopPropagation={()=>{}}>
-		<svg id="close" on:click={()=>close()} viewBox="0 0 12 12">
+		<svg id="close" on:click={()=>close(topDiv)} viewBox="0 0 12 12">
 			<circle cx=6 cy=6 r=6 />
 			<line x1=3 y1=3 x2=9 y2=9 />
 			<line x1=9 y1=3 x2=3 y2=9 />
